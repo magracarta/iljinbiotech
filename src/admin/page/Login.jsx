43848066f22ea16e3150da6511848b7a6a281c0c
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2025-02-23 00:17:08 +0900

    Update Login.jsx

--- src/admin/page/Login.jsx
+++ src/admin/page/Login.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function Login() {
     return (
-        <div>
-            <div className='m-auto max-w-3xl px-20'>
+        <div className='p-24'>
+            <div className='m-auto max-w-3xl px-20 py-20 border border-gray-300 rounded-s-xl'>
                 <div className='text-5xl uppercase text-black'>
                     <h1 className='font-bold text-center'>iljinbiotEch<br/>admin</h1>
                 </div>
-                <div className=''>
-
+                <div className='inputBox'>
+                    
                 </div>
             </div>
 
