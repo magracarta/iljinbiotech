78240cd79e1beb1c8b0bce0fa2577c9b038c7c0d
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2025-02-24 00:17:39 +0900

    FEAT

--- src/admin/component/AddProduct.jsx
+++ src/admin/component/AddProduct.jsx
@@ -1,10 +1,13 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ProductInput from '../ui/ProductInput';
+import { v4 as uuidv4 } from 'uuid';
+
 
 export default function AddProduct() {
-    const [product,setProduct]=useState({title:"",description:""});
+    const [product,setProduct]=useState({title:"", description:"",});
     const [productImg,setProductImg] = useState();
     const [detialimgs, setDetailimgs]= useState([]);
+    const [addProdcut , setAddProduct] = useState(false);
     const pdimginput = useRef(null);
     const detailimginput = useRef(null);
 
@@ -13,7 +16,21 @@ export default function AddProduct() {
         setProduct(prev=>({...prev, title:e.target.value}));
     };
     const detialImgHandle = (e)=>{
+        if( e.target.files[0] === undefined) return;
         setDetailimgs(prev=>[...prev, e.target.files[0]]);
+    }
+    const deletedetailImg =(idx)=>{
+        setDetailimgs(prev=>{
+            prev[idx] = undefined;
+            return prev.filter(el=>el !== undefined);
+        });
+    }
+    useEffect(()=>{
+        if(product.title && product.description && productImg) setAddProduct(true);
+        else setAddProduct(false);
+    },[product, productImg]);
+    const addProductHandler = () => {
+        
     }
 
 
@@ -21,9 +38,8 @@ export default function AddProduct() {
         <div className='py-16 px-9'>
             <h2 className='text-4xl font-bold'>상품 추가</h2>
             <div className='mt-11 flex gap-12'>
-                <div onClick={()=>{pdimginput.current.click();}} className='w-[40%] flex items-center justify-center bg-gray-200 cursor-pointer'>
-                    {productImg?<img className='w-[100%] h-[100%] object-cover' src={URL.createObjectURL(productImg)}/>:<img className='w-[40%]' src='/img/plusIcon.svg' alt='plusIcon.svg'/>}
-                    
+                <div onClick={()=>{pdimginput.current.click();}} className='w-[40%] h-[428px] flex items-center justify-center bg-gray-200 cursor-pointer'>
+                    {productImg?<img className='w-[100%] h-[100%] object-cover' src={URL.createObjectURL(productImg)} alt="productimg"/>:<img className='w-[40%]' src='/img/plusIcon.svg' alt='plusIcon.svg'/>}
                 </div>
                 <div className='w-[60%]'>
                     <ProductInput text={"이름"} name={"title"} handle={(e)=>{setProduct(prev=>({...prev, title:e.target.value}))}} />
@@ -33,16 +49,22 @@ export default function AddProduct() {
                 </div>
             </div>
             <div>
-                {detialimgs&& <ul className='flex justify-between flex-wrap mt-8'>
-                        {detialimgs.map((_el)=><li className='w-[32%] mb-6 border-2 border-gray-200 h-[500px] overflow-hidden'><img className='w-[100%] h-[100%] object-contain' src={URL.createObjectURL(_el)}/></li>)}
-                    </ul>}
+                {detialimgs&& <ul className='flex gap-3 flex-wrap mt-11'>
+                    {detialimgs.map((_el,idx)=>_el&&
+                        <li key={uuidv4()}
+                            className='relative w-[calc(33.33%-8px)] mb-6 border-2 border-gray-200 h-[500px] overflow-hidden group'>
+                            <img className='w-[100%] h-[100%] object-contain' src={URL.createObjectURL(_el)} alt="detailImg"/>
+                            <button className='absolute h-[100%] bg-black bg-opacity-70 bottom-0 left-0 opacity-0 group-hover:opacity-100 transition-all'  onClick={()=>{deletedetailImg(idx)}}>삭제</button>
+                        </li>)
+                    }
+                </ul>}
                 <input accept='image/*' ref={detailimginput} type='file' className='hidden' onChange={detialImgHandle} />
                 <button onClick={()=>{detailimginput.current.click();}} className='flex items-center justify-center opacity-50 hover:opacity-100 mt-16'>
                     <img className='mr-2' src='/img/plusIcon.svg' alt='plusIcon.svg'/>상세 이미지 추가
                 </button>
             </div>
             <div>
-                <button className='notActive w-60 mx-auto mt-12'>상품 추가하기</button>
+                <button onClick={addProductHandler} className={!addProdcut?'notActive w-60 mx-auto mt-12':'w-60 mx-auto mt-12'}>상품 추가하기</button>
             </div>
         </div>
     );
---
+++ src/api/uploader.js
@@ -0,0 +1,34 @@
+const cloudName =  process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
+const uploadPreset = process.env.REACT_APP_CLOUDINARY_API_PRESET;
+
+const url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
+
+
+export async function uploadImage(file){
+    const data = new FormData();
+    data.append('file',file);
+    data.append('upload_preset', uploadPreset);
+    return fetch(url, {
+        method: 'POST',
+        body: data,
+    }).then((res) => res.json())
+    .then((data) => data.url);
+}
+
+
+
+
+
+export async function deleteImage(publicId) {
+    url = `https://api.cloudinary.com/v1_1/${cloudName}/image/destroy`;
+
+    const data = new FormData();
+    data.append("public_id", publicId);
+    data.append("api_key", process.env.REACT_APP_CLOUDINARY_API_KEY); 
+    return fetch(url, {
+        method: "POST",
+        body: data,
+    })
+    .then((res) => res.json())
+    .then((data) => console.log("삭제 결과:", data));
+}
