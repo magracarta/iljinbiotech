b9cb3f960f95728ddbf3daaa8f736f22bbf18099
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2025-02-27 20:34:24 +0900

    Update MainSection01.jsx

--- src/user/component/mainPage/MainSection01.jsx
+++ src/user/component/mainPage/MainSection01.jsx
@@ -4,7 +4,7 @@ export default function MainSection01() {
     return (
         <div className='text-center py-[180px]  max-lg:py-[80px]  max-lg:text-left'>
             <div className='max-w-[1530px] mx-auto px-4'>
-                <h2 className='ani1 title text-4xl  max-lg:text-[6.3vw] text-black font-bold mb-9  max-lg:mb-6'>세상을 변화시키는 기업</h2>
+                <h2 className='ani1 title text-4xl  max-lg:text-[6.3vw] text-black font-bold mb-9  max-lg:mb-6'>인류의건강을 선도하는 바이오기업</h2>
                 <img className='ani2 max-lg:block hidden w-[100%]' src='/img/main/section1_mo.png' alt='section1_mo.png'/>
                 <p className='ani2 mb-16 text-[16px]  max-lg:mt-[30px]  max-lg:mb-0  max-lg:text-[3.7vw]'>일진바이오텍은 인류의 건강과 보다 나은 삶을 위해 세상을 변화시키고 인류를 행복하게 만드는 바이오 회사입니다.<br/>건강기능식품을 연구. 개발. 제조. 전문회사로서 
                     연구개발에서부터 공정 개발. 품질 관리. 임상. 생산에 이르기까지 30여년 이상 축적된 차별화된 R&D 기술과 지속적인 연구개발의 노하우.<br/> 
