ad0b5896084d27c8f29f46cebfcd4dada837471e
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2025-02-27 19:41:32 +0900

    FIX

--- src/user/component/company/CeoGreeting.jsx
+++ src/user/component/company/CeoGreeting.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 export default function CeoGreeting() {
     return (
-        <div className='flex justify-between pt-[87px] items-start max-lg:block  max-lg:pt-[40px] on'>
-            <img className='ani1 w-[40%] max-lg:w-[100%]'
-            src='/img/ceoImg.png' alt='ceoImg.png'/>
-            <div className='w-[60%] pl-[84px] max-lg:w-[100%] max-lg:pl-0'>
+        <div className='flex  justify-between pt-[87px] items-start max-lg:block  max-lg:pt-[40px] on'>
+            <img className='ani1 w-[23%] max-lg:w-[50%] mt-10 rounded-full aspect-square object-cover max-lg:hidden' src='/img/ceoImg.png' alt='ceoImg.png'/>
+
+
+            <div className='w-[75%] pl-[84px] max-lg:w-[100%] max-lg:pl-0'>
                 <div className='pb-[38px] border-b border-[#E6E6E6] max-lg:pt-0 max-lg:border-b-0 max-lg:border-l-4 max-lg:border-black
-                    max-lg:mt-10  max-lg:pb-0 max-lg:mb-[26px] max-lg:pl-5 ani2
+                    max-lg:mt-10  max-lg:pb-0 max-lg:mb-[26px] max-lg:pl-5 ani2 
                 '>
                     <span className='text-[20px] text-black font-medium mb-2 block
                         max-lg:text-base max-lg:font-normal 
@@ -17,22 +18,34 @@ export default function CeoGreeting() {
 
                 <div>
                 <p className='text-[16px] mt-[50px]  max-lg:mt-[0]  max-lg:text-[14px] ani3
-                '>미래의 젊고 활기찬 건강기능 제품을 연구 개발 생산하는 일진바이오텍의 홈페이지를 방문해주셔서 감사의 말씀드립니다.<br/>
-                <br/>
-                어느듯 삼십여년을 향해 달려가고 있는 일진바이오텍은 앞으로도 국내 바이오시장을  넘어 세계적인 <br/>
-                글로벌 시장 개척을 위해 더욱더 연구개발에 분투하겠습니다.<br/><br/>
+                '>
+                 안녕하십니까?<br/><br/>
 
-                대한민국의 생명과학의 우수성을 전 세계에 알리겠습니다. 이에 일진바이오텍의 임직원은 최고의 기업과 제품개발을 위해 고군분투해 나갈것입니다.<br/><br/>
+                미래의 젊고 활기찬 건강기능 제품을 연구 개발 생산하는 일진바이오텍의 홈페이지를 방문해주셔서 감사의 말씀드립니다.<br/><br/>
 
-                앞으로도 지금처럼 저희 회사와 제품을 사랑해주시고 관심 가져주시길 바라며 모든 분들의 평안과 안녕을 기원합니다.<br/><br/>
+                어느듯 삼십여년을 향해 달려가고 있는 일진바이오텍은 인류의 건강하고 .행복한 삶을 향해 앞으로도 국내 바이오시장을  넘어 세계적인 글로벌 시장을 선도하기 위해  더욱더 연구개발에 분투하여 인류건강에 크게 기여하고자 합니다.
+                대한민국의 생명과학의 우수성을 전 세계에 알리는데 힘쓸것이며 혁신적인 기술개발과 사업에 매진할것입니다<br/><br/>
 
-                감사합니다.</p>
+                일진바이오텍은 현재 중대한 변화의 시기를 앞두고 있습니다. 모든 임직원은 세계최고의 바이오제품을 만들겠다는 의지를 가지고 제품개발을 위해 고군분투해 나갈것입니다.<br/><br/>
+
+                회사를 믿고 변함없이 관심을 가져주셔서 다시한번 감사의 말씀 올립니다. 
+                대표이사로서 일진바이오텍의 실적과 새로운 가치향상의 성과로 성공시킬 것을 여러분들께 약속드리며 더 나은 삶을 실현하기 위해 책임과 역할을 다하겠습니다.
+                앞으로도 지금처럼 저희 회사와 제품을 사랑해주시고 관심 가져주시길 바라며 모든 분들의 평안과 행운이 가득하시길 기원합니다.<br/><br/>
+                감사합니다.
+                
+                
+                
+                </p>
                 </div>
                 <div className='flex justify-end items-end mt-[50px] ani4'>
-                    <span className='text-[16px] relative top-[-2px]  max-lg:text-[14px]'>일진바이오텍 대표이사 </span>
+                    <span className='text-[16px] relative top-[-2px]  max-lg:text-[14px]'>주) 일진바이오텍 대표이사 </span>
                     <p className='text-[32px] font-bold text-black ml-3  max-lg:text-[28px]'>김태명</p>
                 </div>
             </div>
+
+
+            
+
         </div>
     );
 }
